327d800af9acdd834ace0665aa9e3a58ceedadd1
2-AK-2 <176363757+2-AK-2@users.noreply.github.com>
2025-11-18 13:13:05 +0530

    fix: Correct Jest config for axios and fix test import path

--- react_ak/frontend/src/__tests__/FareEstimator.test.js
+++ react_ak/frontend/src/__tests__/FareEstimator.test.js
@@ -1,5 +1,5 @@
 import { render, fireEvent, screen } from "@testing-library/react";
-import FareEstimator from "../pages/FareEstimator";
+import FareEstimator from "../pages/Rider/FareEstimator";
 import { MemoryRouter } from "react-router-dom";
 
 test("shows error if fields are empty", () => {
